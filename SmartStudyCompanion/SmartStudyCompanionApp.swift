b66287c08c935f507e086976c1c7d85f6e3e5c86
Yafie <yafie.farabi@student.uts.edu.au>
2026-03-14 13:22:59 +1100

    Initial Commit

---
+++ SmartStudyCompanion/SmartStudyCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartStudyCompanionApp.swift
+//  SmartStudyCompanion
+//
+//  Created by Yafie Farabi on 14/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct SmartStudyCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartStudyCompanionTests/SmartStudyCompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartStudyCompanionTests.swift
+//  SmartStudyCompanionTests
+//
+//  Created by Yafie Farabi on 14/3/2026.
+//
+
+import Testing
+@testable import SmartStudyCompanion
+
+struct SmartStudyCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
